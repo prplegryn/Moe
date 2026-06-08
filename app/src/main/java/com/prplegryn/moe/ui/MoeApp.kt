3a18bb148472a3f1edc5ab88a7933c7b27938ab9
prplegryn <galacticequus@gmail.com>
2026-06-09 03:08:04 +0800

    Fix Compose pointer consume import

--- app/src/main/java/com/prplegryn/moe/ui/MoeApp.kt
+++ app/src/main/java/com/prplegryn/moe/ui/MoeApp.kt
@@ -74,7 +74,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
